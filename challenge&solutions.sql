f05bde77debb2fb59634ad334ae8cedb452b9608
Agustin Musanti <138232526+AgustinMusanti@users.noreply.github.com>
2024-08-31 15:05:54 -0300

    >> challenge&solutions.sql

--- challenge&solutions.sql
+++ challenge&solutions.sql
@@ -7,8 +7,8 @@ use blue_patagon;
            Mostrar: Nombre del Producto, Precio Unitario
 */
 
-        SELECT    producto_nombre AS Nombre_del_Producto,
-                  precio_unitario AS Precio_Unitario
+        SELECT    producto_nombre AS 'Nombre del producto',
+                  precio_unitario AS 'Precio unitario'
 		
         FROM      productos
 		
@@ -20,8 +20,8 @@ use blue_patagon;
 	   Mostrar: Pedido, Cantidad
 */
 
-        SELECT     pedido_id     AS Pedido,
-		   SUM(cantidad) AS Cantidad
+        SELECT     pedido_id     AS 'Pedido',
+		   SUM(cantidad) AS 'Cantidad'
 		
         FROM       pedidos_detalle
 		
@@ -33,7 +33,7 @@ use blue_patagon;
 	   Mostrar: Nombre del Producto
 */
 
-        SELECT  producto_nombre AS Nombre_del_Producto
+        SELECT  producto_nombre AS 'Nombre del producto'
 		
         FROM    productos
 		
@@ -45,9 +45,88 @@ use blue_patagon;
 	   Mostrar: Descuento
 */
 
-        SELECT MAX(descuento) AS Descuento
+        SELECT MAX(descuento) AS 'Descuento'
 		
         FROM   pedidos_detalle;
+
+
+/*
+	5) Obtener el listado de todos los pedidos realizados en el año 2021
+	   Mostrar: Todos los campos de Pedido
+*/
+
+        SELECT      *
+		
+        FROM        pedidos
+		
+        WHERE YEAR  (pedido_fecha) = 1998;
+
+
+/*
+	6) Obtener la cantidad de productos que tiene cada categoría. 
+	   Mostrar: Descripción de la Categoría, Cantidad de Productos
+*/
+
+        SELECT     c.categoria_nombre   AS 'Categoría', 
+		   COUNT(p.producto_id) AS 'Cantidad de productos'
+		
+        FROM       categorias c
+		
+        LEFT JOIN  productos p ON c.categoria_id = p.categoria_id
+		
+        GROUP BY   c.categoria_nombre;
+
+
+/*
+	7) Obtener el listado  de todos los empleados cuyo nombre comienzan con 'T'.
+	   Mostrar: Nombre y Apellido del Empleado (En la misma columna)
+*/
+
+        SELECT CONCAT(e.empleado_nombre, ' ', e.empleado_apellido) AS 'Nombre y apellido del empleado'
+		
+        FROM   empleados e
+		
+        WHERE  e.empleado_nombre LIKE 'T%';
+
+
+/*
+	8) Obtener el nombre del producto cuyo precio unitario sea el mayor
+	   Mostrar: Nombre del Producto, Precio Unitario
+*/
+
+        SELECT    producto_nombre AS 'Nombre del producto', 
+		  precio_unitario AS 'Precio unitario'
+		
+        FROM      productos
+		
+        ORDER BY  precio_unitario DESC
+		
+        LIMIT 1;
+
+
+/*
+	9) Crear un Stored Procedure 'Actualiza_Precio’' que aumente en un 10% el valor de los precios unitarios
+*/
+
+        DELIMITER //
+
+        CREATE PROCEDURE Actualiza_Precio()
+        BEGIN
+        
+        UPDATE productos
+        SET precio_unitario = precio_unitario * 1.10;
+        END //
+
+        DELIMITER ;
+
+        -- Antes de realizar la "call" a este procedimiento almacenado desactivamos el modo de actualización segura temporalmente
+
+        SET SQL_SAFE_UPDATES = 0;
+
+        -- Ahora si podemos ejecutar nuestro procedimiento almacenado
+
+        CALL Actualiza_Precio();
+
 
 
 
